9f0a7c7118d0424e9a76797f5e4d9cf3069af46b
KhuongBaoDuc <khuongbaoduc@gmail.com>
2025-10-19 14:12:33 +0700

    Fix 7 languages

--- preprocess.py
+++ preprocess.py
@@ -14,7 +14,14 @@ LANGUAGES = [
     "Portuguese",
     "Russian",
     "Spanish",
-    "Vietnam"
+    "Vietnam",
+    "Swahil",
+    "Ukraina",
+    "Persian",
+    "Hebrew",
+    "Finnish",
+    "Bengali",
+    "Polish"
 ]
 
 MAX_LEN = 15
@@ -32,6 +39,14 @@ CHARS = (
     "アイウエオカキクケコサシスセソタチツテト"  
     "가나다라마바사아자차카타파하"  
     "ابتثجحخدذرزسشصضطظعغفقكلمنهويءآأؤإئةى"
+    "A Ą B C Ć D E Ę F G H I J K L Ł M N Ń O Ó P R S Ś T U W Y Z Ź Żswah"
+    "б,в,г,ґ,д,ж,з,й,к,л,м,н,п,р,с,т,ф,х,ц,ч,ш,щ,а,е,є,и,і,ї,о,у,ю,я"
+    "B, D, F, G, H, J, K, L, M, N, P, R, S, T, V, W, Y, Z, A, I, E, O, U, Ch, Dh, Gh, Ng', Ny, Sh, Th"
+    "ا‎ ‎ب‎ ‎پ‎ ‎ت‎ ‎ث‎ ‎ج‎ ‎چ‎ ‎ح‎ ‎خ‎ ‎د‎ ‎ذ‎ ‎ر‎ ‎ز‎ ‎ژ‎ ‎س‎ ‎ش‎ ‎ص‎ ‎ض‎ ‎ط‎ ‎ظ‎ ‎ع‎ ‎غ‎ ‎ف‎ ‎ق‎ ‎ک‎ ‎گ‎ ‎ل‎ ‎م‎ ‎ن‎ ‎و‎ ‎ه‎ ‎ی"
+    "א ב ג ד ה ו ז ח ט י כ ל מ נ ס ע פ צ ק ר ש ת"
+    "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z Å Ä Ö"
+    "অ আ ই ঈ উ ঊ ঋ এ ঐ ও ঔ ক খ গ ঘ ঙ চ ছ জ ঝ ঞ ট ঠ ড ঢ ণ ত থ দ ধ ন প ফ ব ভ ম য র ল শ ষ স হ ড় ঢ় য় ৎ ং ঃ ঁ ক খ গ ঘ ঙচ ছ জ ঝ ঞ ট ঠ ড ঢ ণ ত থ দ ধ ন প ফ ব ভ ম য র ল শ ষ স হ ড় ঢ় য় "
+
 )
 CHAR_DICT = {c: i for i, c in enumerate(CHARS)}
 NUM_CLASSES = len(LANGUAGES)
@@ -53,7 +68,7 @@ def load_data(data_folder="data"):
         for w in words:
             X.append(encode_word(w))
             y.append(idx)
-    X = np.array(X)
+    X = np.array(X, dtype=np.float32)
     y = to_categorical(y, NUM_CLASSES)
     return train_test_split(X, y, test_size=0.15, random_state=42)
 
